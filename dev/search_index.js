496041b389b3f7d0f2629863aaabb6025e349488
Documenter.jl <documenter@juliadocs.github.io>
2021-06-25 15:24:02 +0000

    build based on b26e381

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpatialDatasets","category":"page"},{"location":"#SpatialDatasets","page":"Home","title":"SpatialDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpatialDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpatialDatasets]","category":"page"}]
+}
